567d3039cedff4799dbfba5a6dcd10d204e5bd3d
Vindo Kountur <vindokountur@gmail.com>
2020-02-29 20:19:58 +0800

    update no 1

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 let kecepatanBagus = 7;
-let jarakTempuhBagus = 7 * 3600;
+let jarakTempuhBagus = kecepatanBagus * 3600;
 
 let kecepatanIsmail = 10;
 let jarakTempuhIsmail = 0;
@@ -15,6 +15,5 @@ while (jarakTempuhIsmail !== jarakTempuhBagus) {
 let jam = Math.floor(lacakWaktu / 3600);
 let menit = Math.floor(lacakWaktu % 3600 / 60);
 
-// console.log(`Waktu (s) = ${lacakWaktu} detik`);
-// console.log(`Waktu (s) = ${menit} menit`);
 console.log(`Waktu (s) = ${jam} jam, ${menit} menit`);
+console.log(`13:00 + 02:20 => 15:20`);
